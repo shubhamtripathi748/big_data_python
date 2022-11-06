cbfef5c18bbac9e51ea6dcfedeb8a48c8e528ae8
shubhamtripathi748 <62459296+shubhamtripathi748@users.noreply.github.com>
2022-11-06 17:09:31 +0530

    Create HelloW.py

---
+++ HelloW.py
@@ -0,0 +1,44 @@
+#print("Hello World Shubham")
+
+patient_name = 'John Smith'
+patient_age = 20
+patient_is_new = True
+
+#person_name = input('please enter your name ? ')
+#person_like_color = input('which colour you like ? ')
+#print(person_name+" likes "+person_like_color)
+
+#person_weight = input("enter your weight(in pounds ) ? ")
+
+#pounds_to_kg = float(person_weight)*0.45359237
+#print(pounds_to_kg)
+
+string_index = 'Hello Shubham for Python'
+
+#print(string_index[-1])
+#print(string_index[0])
+
+#print(string_index[0:5])
+#print(string_index[1:-1])
+#copy_str = string_index[:]
+#print(copy_str)
+
+
+first = 'Shubham'
+last = 'Tripathi'
+formattedstr = f'{first} {last} is a coder'
+#print(formattedstr)
+title = 'Python for Beginner'
+
+#print(title.upper())
+
+#print(title.lower())
+#print(title.find('B'))
+#print('for' in title)
+
+is_hot = True
+
+if is_hot:
+    print(" it's hot day!!")
+print('Enjoy your day ')
+
